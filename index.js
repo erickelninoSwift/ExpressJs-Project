c044584eaaa47625677d47edf6ec16ce5f8942a0
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-25 22:13:47 +0200

    update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const fileSystem = require('fs').promises;
-const app = express();
 const path = require('path');
+const express = require('express');
+const app = express();
 const AllBicycleData = require('./data/data.json');
-
 const PORT = process.env.PORT || 3000;
+const ejs = require('ejs');
 
 
 
-app.set('view engine','ejs');
+app.set('views','./view');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/',(req,res) =>{
+
+
+
+
+app.get('/', (req,res) =>{
+    
     return res.render('bicycles');
 });
 
@@ -19,11 +25,10 @@ app.get('/bicycle' , (req,res) =>{
     const bicycleShow = AllBicycleData.find(data =>{
         return data.id === req.query.id
     });
-    return res.render('overview');
+    return res.render('overview',req.query);
 });
 
 
 app.listen(PORT,() =>{
     console.log(`Server is running on port ${PORT}`);
-
 })
